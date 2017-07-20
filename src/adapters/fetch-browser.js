29c681e6868ffd9e907b194018758f48adf62879
Philip Schatz <phil@cnx.org>
2017-07-20 17:30:32 -0500

    :bug: window.fetch was not being called properly

--- src/adapters/fetch-browser.js
+++ src/adapters/fetch-browser.js
@@ -1,5 +1,5 @@
 if (typeof window.fetch === 'function') {
-  module.exports = window.fetch
+  module.exports = window.fetch.bind(window)
 } else {
   module.exports = () => {
     throw new Error('Octokat Error: window.fetch function not found. Either use the https://npmjs.com/package/whatwg-fetch polyfill or set Octokat.Fetch variable to be the fetch function')
