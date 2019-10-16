36f053da172dad453e4d1781e2d8dc82a799ae97
Wolfgod778 <52202124+Wolfgod778@users.noreply.github.com>
2019-10-16 01:24:31 -0500

    Delete fetch-browser.js

--- src/adapters/fetch-browser.js
+++
@@ -1,7 +0,0 @@
-if (typeof window.fetch === 'function') {
-  module.exports = window.fetch.bind(window)
-} else {
-  module.exports = () => {
-    throw new Error('Octokat Error: window.fetch function not found. Either use the https://npmjs.com/package/whatwg-fetch polyfill or set Octokat.Fetch variable to be the fetch function')
-  }
-}
